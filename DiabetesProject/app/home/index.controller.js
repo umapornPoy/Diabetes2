8c3d7e87b51498113716495a5c5f3a07f1e02977
umapornPoy <p.tunlang7@gmail.com>
2017-04-26 10:49:45 +0700

    update index

--- DiabetesProject/app/home/index.controller.js
+++ DiabetesProject/app/home/index.controller.js
@@ -19,7 +19,6 @@
             // get current user
 
 
-<<<<<<< HEAD
             UserService.GetCurrent().then(function (user) {
 
                 vm.user = user;
@@ -46,65 +45,7 @@
                 };
                 new Morris.Line(chart);
             });
-=======
-      $(document).ready(function() {
-        //random data
-        var d1 = [
-          [0, 143],
-          [1, 146],
-          [2, 144],
-          [3, 140],
-          [4, 141],
-          [5, 143],
-          [6, 142],
-          [7, 145],
-          [8, 147],
-          [9, 149],
-          [10, 147],
-          [11, 143],
-          [12, 146],
-          [13, 145],
-          [14, 140],
-          [15, 139],
-          [16, 140]
-        ];
-
-        //flot options
-        var options = {
-          series: {
-            curvedLines: {
-              apply: true,
-              active: true,
-              monotonicFit: true
-            }
-          },
-          colors: ["#26B99A"],
-          grid: {
-            borderWidth: {
-              top: 0,
-              right: 0,
-              bottom: 1,
-              left: 1
-            },
-            borderColor: {
-              bottom: "#7F8790",
-              left: "#7F8790"
-            }
-          }
-        };
-        var plot = $.plot($("#placeholder3xx3"), [{
-          label: "Sugar Blood",
-          data: d1,
-          lines: {
-            fillColor: "rgba(150, 202, 89, 0.12)"
-          }, //#96CA59 rgba(150, 202, 89, 0.42)
-          points: {
-            fillColor: "#fff"
-          }
-        }], options);
-      });
   
->>>>>>> 5fa0d712058cf21b07e02b2ce235b95c9ab63e05
         }
 
     }
